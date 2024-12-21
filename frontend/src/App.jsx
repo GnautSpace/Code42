189b85ccc9d4f5aa4ca28c41849584284acebb40
GSHgCH32 <gagana.s2001rs@gmail.com>
2024-12-21 22:18:50 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import {HashRouter as Router,Routes,Route} from "react-router-dom";
+import './App.css';
+
+import Header from  "./components/Header.jsx";
+import SectionPage from "./Pages/SectionPages.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+  return (
+    <> 
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" exact element={<SectionPage/>}/>
+        </Routes>
+        <Footer/>
+      </Router>
+    </>
+  )
+}
+
+export default App;
